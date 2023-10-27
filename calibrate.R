00ec4ed9c1800e021ce42b0e5e67055f1cec06aa
Chris Kypridemos <ckyprid@liverpool.ac.uk>
2023-10-27 20:14:49 +0100

    introduce validate() function

--- calibrate.R
+++ calibrate.R
@@ -7,21 +7,12 @@ scenario_fn_secondary_prevention <- function(sp) NULL
 IMPACTncd$
   del_logs()$
   del_outputs()$
-  calibrate_incd_ftlt(1:10)$ # REMOVE
+  calibrate_incd_ftlt(1:10, replace = FALSE)$
   del_logs()$
   del_outputs()$
-  run(1:10, multicore = TRUE, "sc0")
-
-IMPACTncd$export_summaries(
-  multicore = TRUE,
+  run(1:10, multicore = TRUE, "sc0")$
+  export_summaries(multicore = TRUE,
   type = c(
-    "le", "hle", "dis_char", "prvl",
-    "incd", "dis_mrtl", "mrtl",
-    "allcause_mrtl_by_dis", "cms"
-  )
-)
-
-source("./auxil/process_out.R")
-source("./auxil/simulate_R_validation.r")
-
-
+    "prvl", "incd", "dis_mrtl", "mrtl",
+    "allcause_mrtl_by_dis"))$
+  validate()

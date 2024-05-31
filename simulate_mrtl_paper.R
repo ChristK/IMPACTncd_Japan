9000f5a4fea1f6ab6febb53212ec4fae413984ea
Chris Kypridemos <ckyprid@liverpool.ac.uk>
2024-05-31 11:17:08 +0100

    export qalys and costs for the paper

--- simulate_mrtl_paper.R
+++ simulate_mrtl_paper.R
@@ -644,7 +644,8 @@ IMPACTncd$export_summaries(
     type = c(
         "le", "hle", "dis_char", "prvl",
         "incd", "dis_mrtl", "mrtl",
-        "allcause_mrtl_by_dis", "cms"
+        "allcause_mrtl_by_dis", "cms", 
+        "qalys", "costs"
     )
 )
 

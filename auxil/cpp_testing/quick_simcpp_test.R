e25242c27aba2d46950da80eed65f4cadcfaacce
Chris Kypridemos <ckyprid@liverpool.ac.uk>
2025-08-19 16:34:13 +0100

    feat: add quick testing script for C++ implementations in IMPACT-NCD

---
+++ auxil/cpp_testing/quick_simcpp_test.R
@@ -0,0 +1,26 @@
+## Quick C++ Implementation Testing Script
+## 
+## Simple and fast testing for C++ implementations
+##
+## Usage:
+##   source("scripts/quick_test.R")
+
+library(Rcpp)
+library(data.table)
+library(qs2)
+
+sourceCpp("Rpackage/IMPACTncd_Japan_model_pkg/src/IMPACTncd_sim.cpp") # simcpp() is the function under test
+sourceCpp("auxil/cpp_testing/scripts/IMPACTncd_sim_first_paper.cpp") # simcpp() renamed to simcpp_ref() and is the benchmark
+
+
+qs2::qs_readm("auxil/cpp_testing/test_data/tmp_spfor_test.qs")
+popref <- copy(sp$pop)
+
+
+print("Benchmark timing:")
+print(system.time(simcpp_ref(popref, l, sp$mc)))
+print("Test timing:")
+print(system.time(simcpp(sp$pop, l, sp$mc)))
+
+print("All EQUAL???")
+print(all.equal(popref, sp$pop)) # Check if results are identical

141a826ff7fdfa277428d916d877b14fdb195dbd
Chris Kypridemos <ckyprid@liverpool.ac.uk>
2025-08-20 11:39:48 +0100

    Refactor code structure for improved readability and maintainability

--- auxil/cpp_testing/quick_simcpp_test.R
+++ auxil/cpp_testing/quick_simcpp_test.R
@@ -15,12 +15,16 @@ sourceCpp("auxil/cpp_testing/scripts/IMPACTncd_sim_first_paper.cpp") # simcpp() 
 
 qs2::qs_readm("auxil/cpp_testing/test_data/tmp_spfor_test.qs")
 popref <- copy(sp$pop)
+pop_byyear <- copy(sp$pop)
 
 
 print("Benchmark timing:")
 print(system.time(simcpp_ref(popref, l, sp$mc)))
-print("Test timing:")
+print("Testing timing:")
 print(system.time(simcpp(sp$pop, l, sp$mc)))
+print("Testing by year timing:")
+print(system.time(simcpp_year_based(pop_byyear, l, sp$mc)))
 
 print("All EQUAL???")
 print(all.equal(popref, sp$pop)) # Check if results are identical
+print(all.equal(popref, pop_byyear)) # Check if results are identical
